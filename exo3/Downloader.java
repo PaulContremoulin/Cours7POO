1f34149816a262e36636da74ae9a8f52d708162b
medihebfaiza <fmohamediheb@gmail.com>
2017-05-09 13:04:10 +0200

    exo3

---
+++ exo3/Downloader.java
@@ -0,0 +1,19 @@
+import java.net.URL;
+
+public class Downloader {
+
+	URL url;
+
+	public Downloader(String url){
+		this.url = new URL(url);
+	}
+
+	public void download(String targetFileName){
+		
+	}
+		
+
+
+	
+
+}
